85784ad053aaf0f14b44d7278b6d60d5a5ce28b4
patdelaney <pat_delaney@colonydrive.com>
2015-10-06 17:47:52 -0500

    Updated URLs for help
    Updated URLs for help

--- help/plugin_setup.php
+++ help/plugin_setup.php
@@ -2,12 +2,13 @@
 
 <p>
 Welcome to the Cron Editor Control plugin. <br/>
-Navigate to this page: <a href=“http://www.corntab.com/pages/crontab-gui“>Click here for the Crontab web page</a><br/>
-
+Navigate to this page: http://www.corntab.com/pages/crontab-gui for the Crontab creation tool.</a><br/>
+<br/>
 Specify the minute, hour, days of the month, months, days of the week, and the command you wish to run.<br/>
+<br/>
 Copy the crontab entry that was created by the online tool and paste into the "Add New Cron Job" section of the plugin.<br/>
-
-For more information on how to use crontab, plese refere to this link: <a href=“https://en.wikipedia.org/wiki/Cron“>
+<br/>
+For more information on how to use crontab, go to https://en.wikipedia.org/wiki/Cron.</a><br/>
 
 
 
