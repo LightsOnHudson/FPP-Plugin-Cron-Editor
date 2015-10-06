03288993fb70cc52d1c1e0bae5c1dd9b0079d581
LightsOnHudson <bshaver@MBPRO-TDL.local>
2015-10-06 10:54:36 -0500

    Edit form

--- plugin_setup.php
+++ plugin_setup.php
@@ -33,47 +33,6 @@ if($DEBUG) {
 	
 }
 
-$output = shell_exec('crontab -l');
-
-$cronResult = exec('/usr/bin/crontab -l',$outputResult);
-
-logEntry($cronResult[0]);
-
-
-
-echo $output;
-if(isset($_POST['submit']))
-{
-
-
-	WriteSettingToFile("ENABLED",$ENABLED,$pluginName);
-	//WriteSettingToFile("STATION_ID",urlencode($STATION_ID),$pluginName);
-	WriteSettingToFile("SEPARATOR",urlencode($SEPARATOR),$pluginName);
-
-} else {
-
-	
-
-	$ENABLED = $pluginSettings['ENABLED'];
-	
-	$SEPARATOR = urldecode($pluginSettings['SEPARATOR']);
-	
-	
-}
-
-if(isset($_POST['updatePlugin']))
-{
-	logEntry("updating plugin...");
-	$updateResult = updatePluginFromGitHub($gitURL, $branch="master", $pluginName);
-
-	echo $updateResult."<br/> \n";
-}
-
-//the library keeps repeating the message. send a clear
-
-$LOOPMESSAGE="YES";
-//echo "Enabled: ".$ENABLED."<br/> \n";
-?>
 
 <html>
 <head>
@@ -93,6 +52,47 @@ $LOOPMESSAGE="YES";
 
 <form method="post" action="http://<? echo $_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT']?>/plugin.php?plugin=<? echo $pluginName;?>&page=plugin_setup.php">
 <?php 
+$output = shell_exec('crontab -l');
+$cron_file = "/tmp/crontab.txt";
+if(isset($_POST['add_cron'])) {
+	if(!empty($_POST['add_cron'])) {
+		file_put_contents($cron_file, $output.$_POST['add_cron'].PHP_EOL);
+
+	}
+
+
+
+	if(!empty($_POST['remove_cron'])) {
+		$remove_cron = str_replace($_POST['remove_cron']."\n", "", $output);
+		file_put_contents($cron_file, $remove_cron.PHP_EOL);
+
+	}
+
+	if(isset($_POST['remove_all_cron'])) {
+		echo exec("crontab -r");
+	} else {
+		echo exec("crontab $cron_file");
+
+	}
+	$uri = $_SERVER['REQUEST_URI'];
+	header("Location: $uri");
+	exit;
+
+}
+?>
+<b>Current Cron Jobs:</b><br>
+
+<?
+<?php echo nl2br($output); ?>
+
+<h2>Add or Remove Cron Job</h2>
+<form method="post" action="<?php $_SERVER['REQUEST_URI']; ?>">
+<b>Add New Cron Job:</b><br>
+<input type="text" name="add_cron" size="100" placeholder="e.g.: * * * * * /usr/local/bin/php -q /home/username/public_html/my_cron.php"><br>
+<b>Remove Cron Job:</b><br>
+<input type="text" name="remove_cron" size="100" placeholder="e.g.: * * * * * /usr/local/bin/php -q /home/username/public_html/my_cron.php"><br>
+<input type="checkbox" name="remove_all_cron" value="1"> Remove all cron jobs?<br>
+
 echo "ENABLE PLUGIN: ";
 
 if($ENABLED == "on" || $ENABLED == 1) {
