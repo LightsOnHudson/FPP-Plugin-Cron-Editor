40b3e35cdcd2466f046a4a191e9c1542d81ffe72
patdelaney <pat_delaney@colonydrive.com>
2015-10-06 17:42:09 -0500

    added first pass at crontab help file
    added first pass at crontab help file

--- help/plugin_setup.php
+++ help/plugin_setup.php
@@ -1,7 +1,14 @@
 <b>Cron Editor Control help</b>
 
-
-
 <p>
-Welcome to the Cron Editor Control plugin
+Welcome to the Cron Editor Control plugin. <br/>
+Navigate to this page: <a href=“http://www.corntab.com/pages/crontab-gui“>Click here for the Crontab web page</a><br/>
+
+Specify the minute, hour, days of the month, months, days of the week, and the command you wish to run.<br/>
+Copy the crontab entry that was created by the online tool and paste into the "Add New Cron Job" section of the plugin.<br/>
+
+For more information on how to use crontab, plese refere to this link: <a href=“https://en.wikipedia.org/wiki/Cron“>
+
+
+
 
